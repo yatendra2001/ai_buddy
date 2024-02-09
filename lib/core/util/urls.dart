1c9b7bd1dc7bb894712cd4c376bcb40a7de8e57d
Yatendra Kumar <yatendraroria2001@gmail.com>
2024-02-09 20:06:26 +0530

    fix: changed apikey link

--- lib/core/util/urls.dart
+++ lib/core/util/urls.dart
@@ -3,15 +3,9 @@ import 'package:url_launcher/url_launcher.dart';
 class Urls {
   Urls._();
 
-  static const _termsOfService = 'https://AIBuddy.com/';
-  static const _privacyPolicy = 'https://AIBuddy.com/';
-  static const _buyPro = 'https://AIBuddy.com/';
+  static const _getApiKeyLink = 'https://makersuite.google.com/app/apikey';
 
-  static void showTermsOfService() => _show(_termsOfService);
-
-  static void showPrivacyPolicy() => _show(_privacyPolicy);
-
-  static void showBuyPro() => _show(_buyPro);
+  static void getApiKeyLink() => _show(_getApiKeyLink);
 
   static void _show(String url) {
     launchUrl(Uri.parse(url));
