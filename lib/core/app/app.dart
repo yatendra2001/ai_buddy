fa66fbb6aa7cdf81f06b6ad70a6bbd42ca85d760
Yatendra Kumar <yatendraroria2001@gmail.com>
2024-02-15 13:53:14 +0530

    chores: changed app icon+label

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -8,7 +8,7 @@ class AIBuddy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'AI Buddy App Starter',
+      title: 'AI Buddy',
       theme: darkTheme,
       debugShowCheckedModeBanner: false,
       routerConfig: router,
