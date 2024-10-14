2ef086349fbf5556bb75bebbc174be6c74dfb47e
Yatendra Kumar <yatendraroria2001@gmail.com>
2024-10-14 06:10:16 +0530

    fix: minor reformatting

--- lib/core/config/mellowtel.dart
+++ lib/core/config/mellowtel.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:mellowtel/mellowtel.dart';
 
-final mellowtel = Mellowtel('7478a4e8',
-      appName: 'AI Buddy',
-      appIcon: 'assets/images/app_logo.png',
-      incentive: 'Help us continue building AI Buddy',
-      yesText: '',);
+final mellowtel = Mellowtel(
+  '7478a4e8',
+  appName: 'AI Buddy',
+  appIcon: 'assets/images/app_logo.png',
+  incentive: 'Help us continue building AI Buddy',
+  yesText: '',
+);
 
 Future<void> startMellowtel(
   BuildContext context, {
@@ -15,15 +17,19 @@ Future<void> startMellowtel(
 }) async {
   if (Platform.isIOS) {
     await mellowtel.start(
-        // ignore: use_build_context_synchronously
-        context,
-        onOptIn: () async {},
-        onOptOut: () async {},
-        showConsentDialog: showConsentDialog,);
+      // ignore: use_build_context_synchronously
+      context,
+      onOptIn: () async {},
+      onOptOut: () async {},
+      showConsentDialog: showConsentDialog,
+    );
   }
 }
 
 Future<void> showMellowtelSettings(BuildContext context) async {
-  await mellowtel.showConsentSettingsPage(context,
-      onOptIn: () async {}, onOptOut: () async {},);
+  await mellowtel.showConsentSettingsPage(
+    context,
+    onOptIn: () async {},
+    onOptOut: () async {},
+  );
 }
