7e33e73dea2ebdad88d50fe3d4b578332aca934b
Yatendra Kumar <yatendraroria2001@gmail.com>
2025-02-24 22:37:50 +0530

    fix fonts

--- lib/core/app/style.dart
+++ lib/core/app/style.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:ai_buddy/core/app/app.dart';
 import 'package:ai_buddy/feature/hive/model/chat_bot/chat_bot.dart';
 import 'package:flutter/foundation.dart';
